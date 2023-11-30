fad756466be5f4c1c8bd8e5af00e78b6322c676b
Moh <coding.moh@gmail.com>
2023-11-30 14:45:22 +0000

    reorganized structure

--- 1-docker-hello-world/app.py
+++ 1-docker-hello-world/app.py
@@ -6,8 +6,8 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"hello, happy Krampus"}
+    return {"hello data scientists!!"}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
